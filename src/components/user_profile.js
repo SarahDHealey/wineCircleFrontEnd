3e825ba2d1c6e63923c9e4a4de4315dc949df343
Sarah Healey <s.deane.healey@gmail.com>
2017-04-26 09:10:32 -0600

    profile image placeholder in place

--- src/components/user_profile.js
+++ src/components/user_profile.js
@@ -5,6 +5,7 @@ export default class UserProfile extends React.Component {
   render() {
     return (
       <div className="user-profile">
+        <div className="user one"></div>
         {this.props.children}
       </div>
     );
