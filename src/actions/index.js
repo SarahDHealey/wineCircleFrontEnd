2defd39ba95a6080949984a325ec079c0798ac5e
Sarah Healey <s.deane.healey@gmail.com>
2017-05-03 17:09:10 -0600

    set up route params based on userId

--- src/actions/index.js
+++ src/actions/index.js
@@ -33,7 +33,8 @@ export function signupUser({email, password}) {
     .then(response => {
       dispatch({ type: AUTH_USER });
       localStorage.setItem('token', response.data.token);
-      browserHistory.push('/wine_profile');
+      console.log('from actions response data before push to route', response.data)
+      browserHistory.push('/wine_profile/' + response.data.userId.toString());
     })
     .catch(function (error) {
       dispatch(authError(error.response.data.error));
@@ -52,7 +53,7 @@ export function signinUser({email, password}) {
     .then(response => {
       dispatch({ type: AUTH_USER });
       localStorage.setItem('token', response.data.token);
-      browserHistory.push('/wine_profile');
+      browserHistory.push('/wine_profile/hi');
     })
     .catch(function (error) {
       dispatch(authError(error.response.statusText));
